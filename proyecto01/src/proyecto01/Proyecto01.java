1272a84d8bdfe1a72a495ee640801ac9687d877d
pabloleon <pabloguaman2005@gmail.com>
2025-10-16 12:35:44 -0500

    primer commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -15,6 +15,7 @@ public class Proyecto01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("San Cayetano Alto, C. París, Loja");
     }
     
 }
